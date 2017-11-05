06f5b8a602622f0246e823982bd99f39e3191ff8
yida0301 <804431391@qq.com>
2017-11-05 17:15:29 +0800

    ｙｅｍｉａｎ

--- app/src/main/java/com/example/administrator/demo/MainActivity.java
+++ app/src/main/java/com/example/administrator/demo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv_search=(ImageView)findViewById(R.id.iv_search);
+        iv_search.setOnClickListener(this);
     }
+    public void onClick(View view){
+        Intent intent=new Intent(MainActivity.this,Top_search.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/administrator/demo/Top_search.java
@@ -0,0 +1,16 @@
+package com.example.administrator.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/11/5.
+ */
+
+public class Top_search extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social_top_search);
+    }
+}
