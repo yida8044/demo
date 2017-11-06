9566b32d454e028368885ec2d473b5cc8fb02e46
yida0301 <804431391@qq.com>
2017-11-06 09:35:03 +0800

    ｙｅｍｉａｎ1

--- app/src/main/java/com/example/administrator/demo/MainActivity.java
+++ app/src/main/java/com/example/administrator/demo/MainActivity.java
@@ -14,10 +14,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ImageView iv_search=(ImageView)findViewById(R.id.iv_search);
         iv_search.setOnClickListener(this);
+        ImageView ib_profile=(ImageView)findViewById(R.id.ib_profile);
+        ib_profile.setOnClickListener(this);
+        ImageView ib_contact_list=(ImageView)findViewById(R.id.ib_contact_list);
+        ib_contact_list.setOnClickListener(this);
+
     }
     public void onClick(View view){
-        Intent intent=new Intent(MainActivity.this,Top_search.class);
+        switch(view.getId()){
+            case R.id.iv_search:
+                Intent intent=new Intent(MainActivity.this,Top_search.class);
+                startActivity(intent);
+                break;
+            case R.id.ib_profile:
+                Intent intent1=new Intent(MainActivity.this,MyInfo.class);
+                startActivity(intent1);
+                break;
+            case R.id.ib_contact_list:
+                Intent intent2=new Intent(MainActivity.this,Fragment_contact_list.class);
+                startActivity(intent2);
+                break;
+        }
+      /*  Intent intent=new Intent(MainActivity.this,Top_search.class);
         startActivity(intent);
+        Intent intent1=new Intent(MainActivity.this,MyInfo.class);
+        startActivity(intent1);*/
     }
+
 
 }
--- app/src/main/java/com/example/administrator/demo/MyInfo.java
+++ app/src/main/java/com/example/administrator/demo/MyInfo.java
@@ -1,16 +1,27 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2017/11/5.
  */
 
-public class MyInfo extends AppCompatActivity {
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+public class MyInfo extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myinfo);
+        ImageView iv_back=(ImageView)findViewById(R.id.iv_back);
+        iv_back.setOnClickListener(this);
     }
+        public void onClick(View view){
+            Intent intent=new Intent(MyInfo.this,MainActivity.class);
+            startActivity(intent);
+        }
+
+
 }
